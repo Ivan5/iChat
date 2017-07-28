9574f6f196f933697c97a9a3d364bfe11316b1d6
Ivan5 <converse_ivan@hotmail.com>
2017-07-27 22:51:57 -0500

    Add server file

---
+++ server.js
@@ -0,0 +1,60 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+
+//Connect MongoDB
+mongo.connect('mongodb://127.0.0.1/ichat',(err,db)=>{
+  if (err) {
+    throw err;
+  }
+  console.log('MongoDB connected ');
+
+  //Connect to Socket.io
+  client.on('connection',()=>{
+    let chat = db.collection('chats');
+    //Create fun to send status
+    sendStatus = function(s){
+      client.emit('status',s);
+    }
+
+    //Get chats from mongo collection
+    chat.find().limit(100).sort({_id:1}).toArray((err,res)=>{
+      if (err) {
+        throw err;
+      }
+
+      //Emit the messages
+      client.emit('output',res);
+    });
+
+    //Handle input events
+    client.on('input',(data)=>{
+      let name = data.name;
+      let message = data.message;
+
+      //check for name and message
+      if (name == '' || message == '') {
+        //Send status
+        sendStatus('Please enter a name and message');
+      }else {
+        //insert message
+        chat.insert({name: name,message:message},()=>{
+          client.emit('output',[data]);
+
+          //Send status object
+          sendStatus({
+            message: 'Message send',
+            clear: true
+          });
+        });
+      }
+    });
+    //Headle clear
+    client.on('clear',(data)=>{
+      //Remove all chats
+      chat.remove({},()=>{
+        //Emit cleard
+        client.emit('Clear');
+      });
+    });
+  });
+});
